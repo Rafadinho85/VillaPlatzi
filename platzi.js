972ededcb0b73199e8a6b6059607e9b63bd5dbc2
Rafadinho <rafa85jogo@gmail.com>
2018-08-17 17:42:19 -0600

    creando funciones para agregar nuestros animales

--- platzi.js
+++ platzi.js
@@ -2,13 +2,41 @@ var vp = document.getElementById("villaplatzi");
 var papel = vp.getContext("2d");
 var mapa ="tile.png";
 
-var imagen = new Image();
-imagen.src = mapa;
-imagen.addEventListener("load", dibujar);
+var fondo = new Image();
+fondo.src = mapa;
+fondo.addEventListener("load", dibujar);
+
+var vaca = new Image();
+vaca.src = "vaca.png";
+vaca.addEventListener("load", dibujarvaca);
+
+var cerdo = new Image();
+cerdo.src = "cerdo.png";
+cerdo.addEventListener("load", dibujarcerdo);
+
+var pollo = new Image();
+pollo.src = "pollo.png";
+pollo.addEventListener("load", dibujarpollo);
+
 
 function dibujar ()
 {
-papel.drawImage(imagen, 0, 0);  
+papel.drawImage(fondo, 0, 0);
+}
+
+function dibujarvaca ()
+{
+papel.drawImage(vaca, 10, 10);
+}
+
+function dibujarcerdo ()
+{
+papel.drawImage(cerdo, 20, 20);
+}
+
+function dibujarpollo ()
+{
+papel.drawImage(pollo, 30, 30);
 }
 
 
