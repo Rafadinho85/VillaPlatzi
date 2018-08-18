ba94ccc6fada5e8680271b4386d359dbaafc95d0
Rafadinho <rafa85jogo@gmail.com>
2018-08-17 19:10:40 -0600

    creando objetos json

--- platzi.js
+++ platzi.js
@@ -1,43 +1,44 @@
 var vp = document.getElementById("villaplatzi");
 var papel = vp.getContext("2d");
-var mapa ="tile.png";
 
-var fondo = new Image();
-fondo.src = mapa;
-fondo.addEventListener("load", dibujar);
+var fondo = {
+  url: "tile.png",
+  cargaOK:false
+};
 
-var vaca = new Image();
-vaca.src = "vaca.png";
-vaca.addEventListener("load", dibujarvaca);
+var vaca = {
+  url: "vaca.png",
+  cargaOK:false
+};
 
-var cerdo = new Image();
-cerdo.src = "cerdo.png";
-cerdo.addEventListener("load", dibujarcerdo);
+fondo.imagen = new Image();
+fondo.imagen.src = fondo.url;
+fondo.imagen.addEventListener("load", cargarFondo);
 
-var pollo = new Image();
-pollo.src = "pollo.png";
-pollo.addEventListener("load", dibujarpollo);
+vaca.imagen = new Image();
+vaca.imagen.src = vaca.url;
+vaca.imagen.addEventListener("load", cargarVaca);
 
+function cargarFondo(){
+  fondo.cargaOK =true;
+  dibujar();
+
+}
+
+function cargarVaca(){
+  fondo.cargaOK =true;
+  dibujar();
+}
 
 function dibujar ()
 {
-papel.drawImage(fondo, 0, 0);
+
+  if (fondo.cargaOK) {
+
+  papel.drawImage(fondo.imagen, 0, 0);
+  }
 }
 
-function dibujarvaca ()
-{
-papel.drawImage(vaca, 10, 10);
-}
-
-function dibujarcerdo ()
-{
-papel.drawImage(cerdo, 20, 20);
-}
-
-function dibujarpollo ()
-{
-papel.drawImage(pollo, 30, 30);
-}
 
 
 function aleatorio (min, maxi)
